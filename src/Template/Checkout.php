b260a8c6643c753a7b367481248f9b5756e6fe13
asrarca <asrar.ca@gmail.com>
2025-08-11 17:04:02 -0400

    Changed street address textarea into address line 1 and address line 2 fields.

--- src/Template/Checkout.php
+++ src/Template/Checkout.php
@@ -82,11 +82,15 @@ class Checkout extends Layout
                                 </div>
                             </div>
                             <div class="form-field form-field--required">
-                                <label class="form-field__label" for="billing_address_line1">Street Address: <span class="form-field__label-required"> (required)</span></label>
+                                <label class="form-field__label" for="billing_address_line1">Street Address Line 1: <span class="form-field__label-required"> (required)</span></label>
                                 <div class="form-field__contents">
-                                    <div class="textarea__container">
-                                        <textarea name="billing_address_line1" id="billing_address_line1" class="textarea" rows="2" cols="35"></textarea>
-                                    </div>
+                                    <input name="billing_address_line1" id="billing_address_line1" class="entry" />
+                                </div>
+                            </div>
+                            <div class="form-field form-field--required">
+                                <label class="form-field__label" for="billing_address_line2">Street Address Line 2:</label>
+                                <div class="form-field__contents">
+                                    <input name="billing_address_line2" id="billing_address_line2" class="entry" />
                                 </div>
                             </div>
                             <div class="form-field form-field--required">
